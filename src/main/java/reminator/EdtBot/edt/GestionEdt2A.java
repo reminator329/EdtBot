4eca7bb0bcae6d99bd3c31ea0983611fb15b6ae9
Rémi <rlaborie2000@gmail.com>
2021-06-14 00:02:53 +0200

    Ajout agenda 2A

--- src/main/java/reminator/EdtBot/edt/GestionEdt2A.java
+++ src/main/java/reminator/EdtBot/edt/GestionEdt2A.java
@@ -1,16 +1,63 @@
 package reminator.EdtBot.edt;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import reminator.EdtBot.edt.enums.Edt;
+import reminator.EdtBot.edt.enums.Liens;
+import reminator.EdtBot.utils.HTTPRequest;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.TreeSet;
+
 public class GestionEdt2A extends GestionEdt {
 
+    private String edt2A;
+    private String edt2ATP;
 
+    @Override
+    protected ArrayList<Cours> getNextCourses() {
+
+        Date date = new Date();
+
+        TreeSet<Cours> nextCourses = new TreeSet<>(new TreeSet<>(courses).tailSet(new Cours(null, new Date(date.getTime() - 500 * 3600), null, null)));
+        nextCourses.removeIf(c -> c.isNotAccepted(2));
+        return new ArrayList<>(nextCourses.headSet(nextCourses.first(), true));
+    }
 
     @Override
     protected void updateEdt() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        Date date = new Date();
 
+        String timeMin = dateFormat.format(date).replace(":", "%3A").replace("+", "%2B");
+        String timeMax = dateFormat.format(new Date(date.getTime() + 1000L * 3600 * 24 * 7 * 20 )).replace(":", "%3A").replace("+", "%2B");
+
+        try {
+            this.edt2A = Edt.EDT_2A.getHTTPRequest(timeMin, timeMax).GET();
+            this.edt2ATP = Edt.EDT_2A_TP.getHTTPRequest(timeMin, timeMax).GET();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JSONObject jEdt01 = new JSONObject(edt2A);
+        JSONObject jEdt02 = new JSONObject(edt2ATP);
+
+        JSONArray jCourss01 = jEdt01.getJSONArray("items");
+        JSONArray jCourss02 = jEdt02.getJSONArray("items");
+
+        ajoutCourss(jCourss01, "0");
+        ajoutCourss(jCourss02, "0");
     }
 
     @Override
     protected void updateCsv() {
-
+        try {
+            csv = new HTTPRequest(Liens.CSV_1A.getUrl()).GET();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/reminator/EdtBot/edt/enums/Edt.java
+++ src/main/java/reminator/EdtBot/edt/enums/Edt.java
@@ -6,7 +6,10 @@ public enum Edt {
     EDT01("jjr0au21evqc6guauvan3034ug"),
     EDT02("8nam511995lbsisujjcq80h964"),
     EDT1("4jpbp5hcdimlmov6kscioe4am8"),
-    EDT2("e44ep4hdrj5b2defqf9mmcpd2k");
+    EDT2("e44ep4hdrj5b2defqf9mmcpd2k"),
+    EDT_2A("b99stiti1jc6in7n2cf9buoquk"),
+    EDT_2A_TP("db3arpv5ok1h4sd8rumvgd3cgc"),
+    ;
 
     private final String id;
 
@@ -26,6 +29,26 @@ public enum Edt {
     }
 
     public HTTPRequest getHTTPRequest(String timeMin, String timeMax) {
+        System.out.println(getUrl(timeMin, timeMax));
         return new HTTPRequest(getUrl(timeMin, timeMax));
+        /*
+        .withHeader("Accept", "*//** ")
+        .withHeader("Accept-Encoding", "gzip, deflate, br")
+        .withHeader("Accept-Language", "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3")
+                .withHeader("Alt-Used", "clients6.google.com")
+                .withHeader("Authorization", "SAPISIDHASH 1623620464_45227efa839cdbf6d303f0f297cb46331c46af2f")
+                .withHeader("Connection", "keep-alive")
+                .withHeader("Cookie", "NID=216=C0pWkbCu6m4OwTbFB1n_cf1oVzW98jlWVT66AxGGzzDzMrVg4FsnAcMi0JxZeLXVO0JsEE2Msf3e3lQcQSYcsehSG07F6uTpIdYVBG3WXYSVjnvzcnsmR0EI8iRx-s-jaJ-PX4vJ9Yd1a4IdgeoyJ5-pft-x5ruTdn28h7wXN-ql4H5HI2yOnGMCfnO5INtrOPaT_1010lBMRxqvl88Jyz8Ag0YYnGeZP5rtXfjM6TyaK-9_ldIoGfwZEYch_r64diPhy47kv_8MjKmT4LKsg1jHpajaMk8W66SkOss6T93loHTyrDG74gphFFWWUJfaPCu02dwgwqwoPWkbfcg; CONSENT=YES+FR.fr+V10+BX+950; 1P_JAR=2021-06-13-20; ANID=AHWqTUm8h3a2h7s9D9DWhLlHtm1PrWSHtuQmIVSsJfMWhCghzH9sZUgvIv7hveld; OGPC=19024399-1:; OGP=-19024399:; SID=9w…xpWHHP5HRFnS2rv_76tEPrl6L28mNhq8RoZ0lLLINl6oxaF2nXh5rwUEQ.; __Secure-3PSID=9weAyksyGBCOXxpWHHP5HRFnS2rv_76tEPrl6L28mNhq8RoZGftGFo3w5TxXgm-TjiKoJg.; HSID=AYJlHFUSKV8j2FSsE; SSID=A57om-UxrLOU1QeIS; APISID=7CrbPI91M0f4Gyo3/Ak82x3y2ajbR-7kai; SAPISID=ebY8-c2N9dhr3mL0/AHn-yjoCIgUIODat8; __Secure-3PAPISID=ebY8-c2N9dhr3mL0/AHn-yjoCIgUIODat8; SIDCC=AJi4QfEaT-6mE-I2UCEq9TraB0BXu5j2IcbOMLwwxOaq6_A6qAiphZGL9ARlYsuXES9Hm-t4jF4; __Secure-3PSIDCC=AJi4QfGKVsKA1xnknAgPnaPBxsz2M07xmZlpkeE4dNTzwwSi5E1_uZ25hgBvm0vB4glgNE8MSw")
+                .withHeader("Host", "clients6.google.com")
+                .withHeader("Referer", "https://clients6.google.com/static/proxy.html?usegapi=1&jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.fr.ociYurqUxMU.O%2Fam%3DAQ%2Fd%3D1%2Frs%3DAGLTcCOIwDSigzXEc04K81EinhGJsWNk4A%2Fm%3D__features__")
+                .withHeader("TE", "Trailers")
+                .withHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0")
+                .withHeader("X-ClientDetails", "appVersion=5.0%20(Windows)&platform=Win32&userAgent=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64%3B%20rv%3A89.0)%20Gecko%2F20100101%20Firefox%2F89.0")
+                .withHeader("X-Goog-AuthUser", "0")
+                .withHeader("X-Goog-Encode-Response-If-Executable", "base64")
+                .withHeader("X-JavaScript-User-Agent", "google-api-javascript-client/1.1.0")
+                .withHeader("X-Origin", "https://calendar.google.com")
+                .withHeader("X-Referer", "https://calendar.google.com")
+                .withHeader("X-Requested-With", "XMLHttpRequest");*/
     }
 }
