c529494e3ca425b5e2fbf0e7da023ef850ed6679
Rémi <rlaborie2000@gmail.com>
2021-08-25 16:22:53 +0200

    Fix : Stack comparable

--- src/main/java/reminator/EdtBot/edt/Stack.java
+++ src/main/java/reminator/EdtBot/edt/Stack.java
@@ -1,6 +1,12 @@
 package reminator.EdtBot.edt;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.TreeSet;
 
-public class Stack extends TreeSet<Cours> {
+public class Stack extends TreeSet<Cours> implements Comparable<Stack> {
+    @Override
+    public int compareTo(@NotNull Stack o) {
+        return this.first().compareTo(o.first());
+    }
 }
